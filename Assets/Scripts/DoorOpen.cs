c00c7a7441ca719d966f7242b207557ecb93f165
weirdoofcool <59780020+weirdoofcool@users.noreply.github.com>
2021-10-14 01:07:11 -0400

    Door Opening 2
    - Got door to open big for sprinting
    - Started fixing up door position

--- Assets/Scripts/DoorOpen.cs
+++ Assets/Scripts/DoorOpen.cs
@@ -12,6 +12,7 @@ public class DoorOpen : MonoBehaviour
     public GameObject message;
     public bool buttonPressed = false;
     public bool doorOpens = false;
+    public InputManager im;
     // private InputAction action;
     
     // Start is called before the first frame update
@@ -56,8 +57,14 @@ public class DoorOpen : MonoBehaviour
         {
             maxAngle = 0.0f;
         }
+
+        if(im.isSprinting == true)
+        {
+            maxAngle = 151.55f;
+        }
     }
 
+    #region OpenDoor
     private void OnTriggerStay(Collider collider)
     {
         inArea = true;
@@ -68,17 +75,32 @@ public class DoorOpen : MonoBehaviour
             buttonPressed = true;
         }
 
-        if(inArea == true && buttonPressed == true)
+        if(inArea == true && buttonPressed == true && im.isSprinting == false)
         {
             float angle = Mathf.LerpAngle(minAngle, maxAngle, Time.time);
             transform.eulerAngles = new Vector3(0, angle, 0);
+            door.transform.position = new Vector3(8.57f, 2.1f, 5.02f);
+            
+            
             // door.transform.eulerAngles = new Vector3(0f, -76.942f, 0f);
-            door.transform.position = new Vector3(8.57f, 2.1f, 6.456132f);
             message.SetActive(false);
             doorOpens = true;
         }
+    } //END NORMAL DOOR OPEN
+    #endregion
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if(im.isSprinting == true)
+        {
+            float angle = Mathf.LerpAngle(minAngle, maxAngle, Time.time);
+            transform.eulerAngles = new Vector3(0, angle, 0);
+            door.transform.position = new Vector3(7.3f, 2.1f, 5.55f);
+            message.SetActive(false);
+        }
     }
 
+    #region LeaveDoor
     private void OnTriggerExit(Collider collider)
     {
         // if(inArea == true)
@@ -87,10 +109,11 @@ public class DoorOpen : MonoBehaviour
             transform.eulerAngles = new Vector3(0, angle, 0);
             inArea = false;
             // door.transform.eulerAngles = new Vector3(0f, 0f, 0f);
-            door.transform.position = new Vector3(9.334243f, 2.1f, 6.456132f);
+            door.transform.position = new Vector3(9.334243f, 2.1f, 6.08f);
             buttonPressed = false;
             doorOpens = false;
         // }
         message.SetActive(false);
-    }
+    }   //END LEAVE DOOR TRIGGER
+    #endregion
 }
