c435dedcd277c28eba925c371bdcd92cc98675ec
Matthewdemeo <92183449+Matthewdemeo@users.noreply.github.com>
2021-10-13 23:38:30 -0400

    Deleted old script, added new script
    Redid the script and UI design for the player.

---
+++ Assets/_Testing/Matthew/Matthew_Scripts/InventoryUIManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class InventoryUIManager : MonoBehaviour
+{
+   [SerializeField] private Image inventorySlot1;
+   [SerializeField] private Image inventorySlot2;
+   [SerializeField] private Image inventorySlot3;
+   [SerializeField] private Image inventorySlot4;
+
+}
