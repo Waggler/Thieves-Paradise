b88f4698b8dcfbc34e6334b2c498e423c89fa6c8
shaqgriffin64 <55068788+shaqgriffin64@users.noreply.github.com>
2021-10-22 13:04:14 -0400

    Fixed small bugs

--- Assets/BillboardScript.cs
+++ Assets/BillboardScript.cs
@@ -10,10 +10,7 @@ public class BillboardScript : MonoBehaviour
     private void Awake()
     {
         //Checkin to see if there is already an object assigned to the target var
-        if (target == null)
-        {
-            target = FindObjectOfType<Camera>().gameObject.transform;
-        }
+
 
         //Used to disable or enable the debug text for objects depending on if a bool is true or false
         if (isDebug == true)
