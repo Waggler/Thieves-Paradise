d4364caa1c9f0d3543afbf107320e29003d87fdb
JoeBro218 <70218539+JoeBro218@users.noreply.github.com>
2021-10-31 18:03:52 -0400

    Push Pull work

--- Assets/_Testing/Joe/MasterPushPullScript.cs
+++ Assets/_Testing/Joe/MasterPushPullScript.cs
@@ -4,11 +4,30 @@ using UnityEngine;
 
 public class MasterPushPullScript : MonoBehaviour
 {
+    #region Variables
+    [SerializeField] private bool NearPushPullObject = false;
+    /*
+        bool for checking if crouching, running, or jumping. Call the player movement most likly for that. Call that from the player?
+        Now how do I have the other script come here and to the player?
+    */
+
+    #endregion
+
     void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Push&Pull")
         {
+            print("IN!");
+            NearPushPullObject = true;
+        }
+    }
 
+    void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Push&Pull")
+        {
+            print("OUT!");
+            NearPushPullObject = false;
         }
     }
 }
