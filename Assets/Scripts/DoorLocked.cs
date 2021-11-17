2d0003c37a9e60ce040652009341a83a0527fdf6
weirdoofcool <59780020+weirdoofcool@users.noreply.github.com>
2021-11-17 00:11:22 -0500

    Cleaning Before the Purge
    Made prefabs of doors and vents and key. Locked door can open now

--- Assets/Scripts/DoorLocked.cs
+++ Assets/Scripts/DoorLocked.cs
@@ -7,9 +7,22 @@ public class DoorLocked : MonoBehaviour
     //message1 is the message that talks about the locked door
     public GameObject door;
     public bool isLocked = true;
+    public bool inArea = false;
     public GameObject message1;
     public GameObject message2;
     public Key key;
+    public bool button1Pressed = false;
+    public bool button2Pressed = false;
+    public bool button3Pressed = false;
+    public bool doorOpens = false;
+    public PlayerMovement pm;
+    public InputManager im;
+    public bool crouchOpen1 = false;
+    public bool crouchOpen2 = false;
+    public bool crouchOpen3 = false;
+    public int buttonPressed = 0;
+    public int buttonPressed2 = 0;
+    public int buttonPressed3 = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -18,12 +31,110 @@ public class DoorLocked : MonoBehaviour
         message2.SetActive(false);
     }
 
+    float minAngle = 0.0f;
+    float maxAngle = 0.0f;
+
     // Update is called once per frame
     void Update()
     {
         if(key.gotKey == true)
         {
             isLocked = false;
+        }
+
+        if(doorOpens == true)
+        {
+            maxAngle = -90f;
+        }
+
+        if(im.isSprinting == true)
+        {
+            maxAngle = -151.55f;
+        }
+
+        if(crouchOpen1 == true)
+        {
+            buttonPressed++;
+            message2.SetActive(false);
+        }
+
+        if(buttonPressed > 1)
+        {
+            buttonPressed = 1;
+        }
+
+        if(buttonPressed == 1)
+        {
+            maxAngle = -37.06f;
+            // buttonPressed = 0;
+        }
+
+        if(crouchOpen2 == true)
+        {
+            buttonPressed2++;
+        }
+
+        if(buttonPressed2 > 1)
+        {
+            buttonPressed2 = 1;
+        }
+
+        if(buttonPressed2 == 1)
+        {
+            maxAngle = -48.15f;
+        }
+
+        if(crouchOpen3 == true)
+        {
+            buttonPressed3++;
+        }
+
+        if(buttonPressed3 > 1)
+        {
+            buttonPressed3 = 1;
+        }
+
+        if(buttonPressed3 == 1)
+        {
+            maxAngle = -71.3f;
+        }
+    }
+
+    private void OnTriggerStay(Collider collider)
+    {
+        inArea = true;
+
+        if(im.isSprinting == true)
+        {
+            message2.SetActive(false);
+        }
+
+        if(Input.GetKey("e") && im.isCrouching == true)
+        {
+            crouchOpen1 = true;
+        }
+
+        if(Input.GetKey("e"))
+        {
+            button1Pressed = true;
+        }
+
+        if(Input.GetKey("e") && buttonPressed == 1)
+        {
+            crouchOpen2 = true;
+        }
+
+        if(Input.GetKey("e") && buttonPressed2 == 1)
+        {
+            crouchOpen3 = true;
+        }
+
+        if(inArea == true && button1Pressed == true && im.isSprinting == false && isLocked == false)
+        {
+            float angle = Mathf.LerpAngle(minAngle, maxAngle, Time.time);
+            door.transform.eulerAngles = new Vector3(0, angle, 0);
+            message2.SetActive(false);
+            doorOpens = true;
         }
     }
 
@@ -37,6 +148,14 @@ public class DoorLocked : MonoBehaviour
         {
             message2.SetActive(true);
         }
+
+        if(im.isSprinting == true && isLocked == false)
+        {
+            float angle = Mathf.LerpAngle(minAngle, maxAngle, Time.time);
+            door.transform.eulerAngles = new Vector3(0, angle, 0);
+            // door.transform.position = new Vector3(7.3f, 2.1f, 7.16f);
+            message2.SetActive(false);
+        }
     }
 
     void OnTriggerExit()
@@ -49,5 +168,19 @@ public class DoorLocked : MonoBehaviour
         {
             message2.SetActive(false);
         }
+
+        float angle = Mathf.LerpAngle(maxAngle, minAngle, Time.time);
+        door.transform.eulerAngles = new Vector3(0, angle, 0);
+        inArea = false;
+        button1Pressed = false;
+        button2Pressed = false;
+        button3Pressed = false;
+        doorOpens = false;
+        crouchOpen1 = false;
+        crouchOpen2 = false;
+        crouchOpen3 = false;
+        buttonPressed = 0;
+        buttonPressed2 = 0;
+        buttonPressed3 = 0;
     }
 }
