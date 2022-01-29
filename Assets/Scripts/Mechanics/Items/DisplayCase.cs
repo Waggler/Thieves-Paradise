2e61885e3a156b092d1b4393efa11a0b51587a49
weirdoofcool <59780020+weirdoofcool@users.noreply.github.com>
2022-01-28 23:46:16 -0500

    Dislay Case Beginnings

--- Assets/Scripts/Mechanics/Items/DisplayCase.cs
+++ Assets/Scripts/Mechanics/Items/DisplayCase.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class DisplayCase : MonoBehaviour
 {
+    public GameObject caseDoor;
+    public Key key;
+    public bool isLocked = true;
+    public bool isOpened = false;
+    public PlayerMovement pm;
+    public InputManager im;
+    public bool inArea = false;
+    public float doorOpen;
+    public float Timer = 0f;
+    public bool reachedTimer = false;
+    float minAngle = 0.0f;
+    float maxAngle = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +26,14 @@ public class DisplayCase : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(key.gotKey == true)
+        {
+            isLocked = false;
+        }
+
+        if(isOpened == true)
+        {
+            maxAngle = doorOpen;
+        }
     }
 }
