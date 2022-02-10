8c5ac7b517b9f5c42524b174fe4ffdf5078b669c
pwilshere <pwilshere2010@my.fit.edu>
2022-02-09 23:03:22 -0500

    added cheat codes

---
+++ Assets/_Testing/Patrick/Scripts/CheatCodes.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class CheatCodes : MonoBehaviour
+{
+    
+    public GameObject cheatMenu;
+    public TextMeshProUGUI godText;
+    private bool cheatsEnabled;
+    private bool isGod;
+    
+    
+
+    void Awake()
+    {
+        cheatMenu.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (cheatsEnabled)
+        {
+            if (Input.GetKeyDown(KeyCode.G))
+            {
+                ToggleGodMode();
+            }
+        }
+    }
+
+    public void ShowMenu()
+    {
+        if (cheatsEnabled)
+        {
+            cheatMenu.SetActive(false);
+            cheatsEnabled = false;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            DontDestroyOnLoad(this.gameObject);
+        } else
+        {
+            cheatMenu.SetActive(true);
+            cheatsEnabled = true;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+    public void LoadScene(int scene)
+    {
+        ShowMenu();
+        SceneManager.LoadScene(scene);
+    }
+
+    public void ToggleGodMode()
+    {
+        print("Attempting to Set God Mode");
+        PlayerMovement player = (PlayerMovement)FindObjectOfType(typeof(PlayerMovement));
+        if (player == null)
+        {
+            print("Player Not Found");
+            return;
+        }
+        if (player.hp < 5)
+        {
+            print("God Mode Enabled");
+            player.hp = 99999;
+            godText.text = "God Mode Enabled (G)";
+        }else
+        {
+            godText.text = "God Mode Disabled (G)";
+            player.hp = 2;
+        }
+    }
+}
