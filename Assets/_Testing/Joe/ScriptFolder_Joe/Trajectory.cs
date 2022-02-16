80e487949c94e0333c8b93be905fb04f186a27d8
JoeBro218 <70218539+JoeBro218@users.noreply.github.com>
2022-02-16 16:01:20 -0500

    Updated work

--- Assets/_Testing/Joe/ScriptFolder_Joe/Trajectory.cs
+++ Assets/_Testing/Joe/ScriptFolder_Joe/Trajectory.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static System.Math;
 
 public class Trajectory : MonoBehaviour
 {
+    [SerializeField] private float Velocity;
+    [SerializeField] private float AngleofLaunch;
+    [SerializeField] private float InitialHeight;
+    private float y;
+    private float x;
+    private float Vy;
+    private float Vx;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
