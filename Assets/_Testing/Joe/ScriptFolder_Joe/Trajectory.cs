b61e3e134e97cbfa547e614fdeda849a1dfabc65
JoeBro218 <70218539+JoeBro218@users.noreply.github.com>
2022-02-22 16:07:49 -0500

    Nice update (Thank you Nick)

--- Assets/_Testing/Joe/ScriptFolder_Joe/Trajectory.cs
+++ Assets/_Testing/Joe/ScriptFolder_Joe/Trajectory.cs
@@ -5,20 +5,38 @@ using static System.Math;
 
 public class Trajectory : MonoBehaviour
 {
+    [Header("Calulation Variables")]
+    [SerializeField] private Transform InitialPosition;
     private InventoryController inventoryController;
     private float Velocity;
     private float AngleofLaunch = 45.0f;
-    
-    [SerializeField] private Transform InitialPosition;
 
-    private void Start()
+    [Header("Visualization")]
+    [SerializeField] private LineRenderer Line;
+    [SerializeField] private int LineLength;
+    [SerializeField] private Material Mat1, Mat2, Mat3;
+
+    void Start()
     {
         inventoryController = GetComponent<InventoryController>();
     }
 
-    private void Update()
+    void Update()
     {
         Velocity = inventoryController.throwForce;
+
+    }
+
+    void Visulization()
+    {
+        for(int i = 0; i < LineLength; i++)
+        {
+
+        }
+    }
+
+    void Calculations()
+    {
 
     }
 }
