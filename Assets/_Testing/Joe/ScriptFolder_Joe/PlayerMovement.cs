617e3b93e4ae996f9a9f58552bfec23bb94a7e02
Waggler <khdavis4@gmail.com>
2022-02-27 13:32:51 -0500

    Update PlayerMovement.cs

--- Assets/_Testing/Joe/ScriptFolder_Joe/PlayerMovement.cs
+++ Assets/_Testing/Joe/ScriptFolder_Joe/PlayerMovement.cs
@@ -160,7 +160,7 @@ public class PlayerMovement : MonoBehaviour
             gameController = FindObjectOfType<GameController>();
         }
 
-        transform.position = GameController.gameControllerInstance.lastCheckPoint;
+        //transform.position = GameController.gameControllerInstance.lastCheckPoint;
     }
 
     void Update()
