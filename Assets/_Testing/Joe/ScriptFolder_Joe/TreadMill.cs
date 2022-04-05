05840d3c12b2db3d0474688d89079ac019d28e9c
JoeBro218 <70218539+JoeBro218@users.noreply.github.com>
2022-04-05 16:01:30 -0400

    Treadmill

--- Assets/_Testing/Joe/ScriptFolder_Joe/TreadMill.cs
+++ Assets/_Testing/Joe/ScriptFolder_Joe/TreadMill.cs
@@ -4,17 +4,31 @@ using UnityEngine;
 
 public class TreadMill : MonoBehaviour
 {
-    Vector3 Direction;
+    [Header("Direction & Speed.")]
+    [Tooltip("Use only the X and Z fields as Y will send you to the moon. Use either -1 or 1 for the fields. Never have more than on feild active; example (1, 0, 1) will cause the player to move diagonal.")]
+    [SerializeField] private Vector3 Direction;
+    [SerializeField] private float TreadMillSpeed;
 
-    // Start is called before the first frame update
-    void Start()
+    [Header("")]
+    [SerializeField] private CharacterController Player;
+
+    private void OnTriggerStay(Collider other)
     {
-        
+        if(other.gameObject.tag == "Player")
+        {
+            Player.Move(Direction * TreadMillSpeed * Time.deltaTime);
+            TreadMillSpeed += Time.deltaTime;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit(Collider other)
     {
-        
+        if (other.gameObject.tag == "Player")
+        {
+            Player.Move(Direction * 15 * Time.deltaTime * 2);
+            TreadMillSpeed += Time.deltaTime;
+
+            TreadMillSpeed = 0;
+        }
     }
 }
