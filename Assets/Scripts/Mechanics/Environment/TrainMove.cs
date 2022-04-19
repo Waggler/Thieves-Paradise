cf53fb2c74571ed96d51f4c575f3335ce8c389ad
weirdoofcool <59780020+weirdoofcool@users.noreply.github.com>
2022-04-19 15:30:31 -0400

    Train Progress

--- Assets/Scripts/Mechanics/Environment/TrainMove.cs
+++ Assets/Scripts/Mechanics/Environment/TrainMove.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class TrainMove : MonoBehaviour
 {
+    public List<Transform> waypoints = new List<Transform>();
+    private Transform targetWaypoint;
+    private int targetWaypointIndex = 0;
+    private float minDistance = 0.1f;
+    private int lastWaypointIndex;
+
+    private float movementSpeed = 3.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        targetWaypoint = waypoints[targetWaypointIndex];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float movementStep = movementSpeed * Time.deltaTime;
+
+        transform.position = Vector3.MoveTowards(transform.position, targetWaypoint.position, movementStep);
     }
 }
